085b6aec5ec3be3224041d2e3cb825aa6f40404e
Gyrov Bogdan <gyrovbogdan@gmail.com>
2024-03-06 18:07:19 +0300

    Add images in public folder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,9 @@
 
 
 @push('stylesheets')
-    <link rel="stylesheet" href="{{ asset('css/welcome-carousel.css', false) }}">
-    <link rel="stylesheet" href="{{ asset('css/features.css', false) }}">
-    <link rel="stylesheet" href="{{ asset('css/welcome-cards.css', false) }}">
+    <link rel="stylesheet" href="{{ asset('css/welcome-carousel.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/features.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/welcome-cards.css') }}">
 @endpush
 
 @section('content')
