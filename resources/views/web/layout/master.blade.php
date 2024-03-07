c5a0a99cd540beb20cd8ed12094e620e8caa1de6
Gyrov Bogdan <gyrovbogdan@gmail.com>
2024-03-07 14:30:53 +0300

    Add loading bootstrap with Vite

--- resources/views/web/layout/master.blade.php
+++ resources/views/web/layout/master.blade.php
@@ -8,8 +8,9 @@
     <title>
         @yield('title', 'Климат и точка Крым')
     </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
     <link rel="stylesheet" href="{{ asset('css/app.css') }}?v1.0">
     @stack('stylesheets')
 </head>
@@ -26,11 +27,6 @@
 
     @include('web.layout.partials.footer')
     @section('scripts')
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-            < /body>
-        </script>
-        <script src="{{ asset('js/jquery.min.js') }}"></script>
         <script src="{{ asset('js/app.js') }}"></script>
     @show
 
