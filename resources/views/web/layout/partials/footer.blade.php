60c3325c7c7d7e44cef020414a48973808b741c1
Gyrov Bogdan <gyrovbogdan@gmail.com>
2024-03-23 12:28:39 +0300

    Add aria labels to links

--- resources/views/web/layout/partials/footer.blade.php
+++ resources/views/web/layout/partials/footer.blade.php
@@ -2,20 +2,22 @@
     <footer class="py-3 my-4 border-top">
         <div class="d-flex flex-wrap justify-content-between">
             <div class="col-md-4 d-flex">
-                <a class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" href="tel:{{ $phone }}">
+                <a class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" href="tel:{{ $phone }}"
+                    aria-label="phone-number">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="24" fill="currentColor"
                         class="bi bi-telephone" viewBox="0 0 16 16">
                         <path
                             d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
                     </svg>
                 </a>
-                <a class="mb-3 mb-md-0 pt-1 text-body-secondary" href="tel:{{ $phone }}">{{ $phone }}</a>
+                <a class="mb-3 mb-md-0 pt-1 text-body-secondary" href="tel:{{ $phone }}"
+                    aria-label="phone-number">{{ $phone }}</a>
 
             </div>
 
             <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
 
-                <li class="ms-3"><a class="text-body-secondary" href="{{ $vk }}"><svg
+                <li class="ms-3"><a class="text-body-secondary" href="{{ $vk }}" aria-label="vk-link"><svg
                             xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0,0,256,256">
                             <g fill="currentColor" fill-rule="nonzero" stroke="none" stroke-width="1"
                                 stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray=""
@@ -29,16 +31,17 @@
                             </g>
                         </svg></a></li>
 
-                <li class="ms-3"><a class="text-body-secondary" href="{{ $whatsApp }}"><svg
-                            xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                            class="bi bi-whatsapp" viewBox="0 0 16 16">
+                <li class="ms-3"><a class="text-body-secondary" href="{{ $whatsApp }}"
+                        aria-label="whatsApp-link"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                            fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
                             <path
                                 d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
-                        </svg></a></li>
+                        </svg>
+                    </a></li>
 
-                <li class="ms-3"><a class="text-body-secondary" href="{{ $telegram }}"><svg
-                            xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                            class="bi bi-telegram" viewBox="0 0 16 16">
+                <li class="ms-3"><a class="text-body-secondary" href="{{ $telegram }}"
+                        aria-label="telegram-link"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                            fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">
                             <path
                                 d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09" />
                         </svg></a></li>
