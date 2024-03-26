47a7f21d4faa9ee9fc1625810cbd0a3d4bb43a2c
Gyrov Bogdan <gyrovbogdan@gmail.com>
2024-03-26 18:22:23 +0300

    Seo changes and smooth loading

--- resources/views/web/layout/partials/meta.blade.php
+++ resources/views/web/layout/partials/meta.blade.php
@@ -7,6 +7,7 @@
 <meta property="og:description" content="@yield('og:description', $ogDescription)">
 <meta property="og:url" content="@yield('og:url', $ogUrl)">
 <meta property="og:image" content="{{ Vite::asset('resources/images/conditioner.png') }}">
+<meta name="yandex-verification" content="49036e9814867f4b" />
 <meta property="og:site_name"
     content="Климат и точка Крым | Кондиционеры в Симферополе | Продажа, установка, ремонт | Купить кондиционер | Установить кондиционер | Недорогие кондиционеры | Климат и Точка Крым">
 <meta property="og:locale" content="ru_RU">
