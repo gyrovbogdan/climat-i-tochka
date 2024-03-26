ff9c5d274f339d32e591c63d34dddb10da034b51
Gyrov Bogdan <gyrovbogdan@gmail.com>
2024-03-26 20:55:44 +0300

    Fix quotes

--- resources/views/web/layout/master.blade.php
+++ resources/views/web/layout/master.blade.php
@@ -6,7 +6,7 @@
     <title>
         @yield('title', 'Кондиционеры в Симферополе | Продажа, установка, ремонт | Купить кондиционер | Установить кондиционер | Недорогие кондиционеры | Климат и Точка Крым')
     </title>
-    <link rel=”icon” href=”/favicon.ico” type=”image/x-icon”>
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
