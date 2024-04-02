b35f63a644c4f35aa56b820729de915978b43b64
Gyrov Bogdan <gyrovbogdan@gmail.com>
2024-04-02 10:30:53 +0300

    Add google site verification

--- resources/views/web/layout/partials/meta.blade.php
+++ resources/views/web/layout/partials/meta.blade.php
@@ -8,6 +8,7 @@
 <meta property="og:url" content="@yield('og:url', $ogUrl)">
 <meta property="og:image" content="{{ Vite::asset('resources/images/conditioner.png') }}">
 <meta name="yandex-verification" content="49036e9814867f4b" />
+<meta name="google-site-verification" content="vYdV_QaajRzyIkxFFW7guCgq8E0752h9adLzOs6hyNw" />
 <meta property="og:site_name"
     content="Климат и точка Крым | Кондиционеры в Симферополе | Продажа, установка, ремонт | Купить кондиционер | Установить кондиционер | Недорогие кондиционеры | Климат и Точка Крым">
 <meta property="og:locale" content="ru_RU">
