2fa943c9af07171b02d3dee201b4d4e22ae6a697
Gyrov Bogdan <gyrovbogdan@gmail.com>
2024-04-14 15:30:04 +0300

    Remove unused code

--- resources/views/web/layout/master.blade.php
+++ resources/views/web/layout/master.blade.php
@@ -12,13 +12,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     @vite(['resources/sass/app.scss', 'resources/js/app.js', 'public/css/app/app.css', 'public/css/app/partials/navbar.css'])
     @stack('stylesheets')
-    <noscript>
-        <style>
-            .body_hide {
-                opacity: 1 !important;
-            }
-        </style>
-    </noscript>
 </head>
 
 <body>
